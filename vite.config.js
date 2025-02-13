6e1fd2a49d303b7ea8cb0e825393619944cc0f4f
Steve Ryan <syntithenai@gmail.com>
2025-02-13 21:04:28 +1100

    updatebaseurl

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: './',
+  build: {
+    outDir: './docs',
+    emptyOutDir: true, // also necessary
+  }
 })
+

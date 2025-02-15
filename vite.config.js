40d81b6ad9fe78733f8d575d693fb02a814f657c
Steve Ryan <syntithenai@gmail.com>
2025-02-15 11:51:21 +1100

    build

--- vite.config.js
+++ vite.config.js
@@ -18,13 +18,19 @@ export default defineConfig({
         theme_color: '#000000',
         icons: [
           {
-            src: '/icon-192.png',
+            src: '/canberraoldtimepickersandfiddlers/icon-192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/icon-512.png',
+            src: '/canberraoldtimepickersandfiddlers/icon-512.png',
             sizes: '512x512',
+            type: 'image/png'
+          }
+          ,
+          {
+            src: '/canberraoldtimepickersandfiddlers/icon-144.png',
+            sizes: '144x144',
             type: 'image/png'
           }
         ]

d17ad70b8da707c9466b632ce6a46f7c55eb2a44
Steve Ryan <syntithenai@gmail.com>
2025-02-16 12:43:49 +1100

    fix progress rendering

--- src/App.jsx
+++ src/App.jsx
@@ -222,7 +222,7 @@ const App = () => {
 
   function formatTime(seconds) {
     const minutes = Math.floor(seconds / 60);
-    const remainingSeconds = seconds % 60;
+    const remainingSeconds = Math.floor(seconds % 60);
     return `${minutes}:${remainingSeconds.toString().padStart(2, '0')}`;
   }
   
